b801f4af6fc172dd5d0c630382449bfe919b6a25
khalilmasri <khalil.1996km@gmail.com>
2022-04-24 18:31:30 +0300

    Main

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description: my_ftps
+ *
+ *        Version:  1.0
+ *        Created:  04/24/2022 03:24:22 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Khalil Masree, Chris Dallat
+ *   Organization:  European Leadership univirsity 
+ *
+ * =====================================================================================
+ */
+
+#include "../include/exception.hpp"
+#include "../include/server.hpp"
+
+int main(int argc, char **argv){
+
+    /* try{ */
+    /*     if(argc!=1) */
+    /*         throw(ArgsException()); */
+    /* }catch(ArgsException &err){ */
+    /*     std::cout << err.what() << std::endl; */
+    /*     exit(-1); */
+    /* } */
+
+    Server server;
+
+    return 0;
+}
+
