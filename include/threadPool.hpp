971b19abe3d002819929cbb288ca53a42ee0ed8c
khalilmasri <khalil.1996km@gmail.com>
2022-05-25 21:01:23 +0300

    ThreadPool

--- include/threadPool.hpp
+++ include/threadPool.hpp
@@ -5,20 +5,24 @@
 #include <queue>
 #include <mutex>
 #include <thread>
+#include <functional>
+#include <condition_variable>
 
 class ThreadPool {
     public:
         ThreadPool();
+        ThreadPool(const int);
         ~ThreadPool();
-
+        
         void Start();
         void QueueJob(const std::function<void()>& job);
         void Stop();
-        void busy();
-
-    private:
+        bool busy();
         void ThreadLoop();
 
+    private:
+
+        int server_sock;
         bool should_terminate = false;           // Tells threads to stop looking for jobs
         std::mutex queue_mutex;                  // Prevents data races to the job queue
         std::condition_variable mutex_condition; // Allows threads to wait on new jobs or termination 
