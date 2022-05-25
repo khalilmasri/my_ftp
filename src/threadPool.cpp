49679863f29685a6bb5c7e7d1a1baf90cc87f839
khalilmasri <khalil.1996km@gmail.com>
2022-05-25 21:01:48 +0300

    ThreadPool

--- src/threadPool.cpp
+++ src/threadPool.cpp
@@ -1,17 +1,26 @@
-#include "threadPool.hpp"
-#include "server.hpp"
+#include "../include/threadPool.hpp"
+#include "../include/server.hpp"
 
-ThreadPool::ThreadPool() {
+ThreadPool::ThreadPool() {}
+
+ThreadPool::ThreadPool(const int server_sock) {
     const uint32_t num_threads = std::thread::hardware_concurrency() / 2; // Max # of threads the system supports
     threads.resize(num_threads);
-    std::cout << num_threads;
+    this->server_sock = server_sock;
     for (uint32_t i = 0; i < num_threads; i++) {
-        threads.at(i) = std::thread(ThreadLoop);
+        threads.push_back(std::thread(&ThreadPool::ThreadLoop, this));
     }
-    std::cout << "num of thread created = " << num_threads;
+
+    std::cout << "num of thread created = " << num_threads << std::endl;
+    
+    for(int i = 0; i < num_threads; i++){
+        std::cout << "JOINING\n";
+        threads.at(i).join();
+    }
 }
-ThreadPool::ThreadLoop(void) {
-  while (true) {
+
+void ThreadPool::ThreadLoop() {
+    while (true) {
         //The anonymous code blocks are used so that when they are exited, 
         //the std::unique_lock variables created within them go out of scope, unlocking the mutex.
         std::function<void()> job;
@@ -22,23 +31,25 @@ ThreadPool::ThreadLoop(void) {
             struct sockaddr_in client_addr;
             socklen_t addr_size = sizeof(client_addr);
             int count = 0;
+            std::cout << "HERE MALAKA\n";
 
             if ((request_accept = accept(this->server_sock, (struct sockaddr*)&client_addr, &addr_size)) < 0)
             {
                 std::cout << "accept Error\n";
                 exit(-1);
             }
+            std::cout << "IM AFTER ACCEPT\n>";
             std::cout << "Client Addr = " << client_addr.sin_addr.s_addr << std::endl;
 
             bytes = read(request_accept, &buff, MAX_TRANSMISSION_LENGTH);
 
             std::unique_lock<std::mutex> lock(queue_mutex);
             mutex_condition.wait(lock, [this] {
-                return !jobs.empty() || should_terminate;
-            });
-            if (should_terminate) {
-                return;
-            }
+                    return !jobs.empty() /*|| should_terminate*/;
+                    });
+            /* if (should_terminate) { */
+            /*     return; */
+            /* } */
             job = jobs.front();
             jobs.pop();
         }
@@ -50,7 +61,7 @@ ThreadPool::ThreadLoop(void) {
 ThreadPool::~ThreadPool() {};
 
 void ThreadPool::QueueJob(const std::function<void()>& job) {
-      {
+    {
         std::unique_lock<std::mutex> lock(queue_mutex);
         jobs.push(job);
     }
@@ -69,8 +80,8 @@ void ThreadPool::Stop() {
     threads.clear();
 }
 
-void ThreadPool::busy() {  
-    bool poolbusy;
+bool ThreadPool::busy() {  
+    bool poolbusy = false;
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
         poolbusy = jobs.empty();
