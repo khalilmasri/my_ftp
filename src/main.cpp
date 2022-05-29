12956e71fb6a4f764b6fa9c178cbe9bf1352ac85
khalilmasri <khalil.1996km@gmail.com>
2022-05-29 15:01:51 +0300

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -18,34 +18,33 @@
 
 #include "../include/exception.hpp"
 #include "../include/server.hpp"
+#include "../include/client.hpp"
 #include <string>
 #include "../include/threadPool.hpp"
 
 
 int main(int argc, char **argv){
 
+    // CREATE AND RUN THE SERVER
     Server server;
-    server.run();
-
-    ThreadPool Th;
-    Th.Start();
-
     
-    while(!Th.getExit()){
-            // int request_accept;
-            // struct sockaddr_in client_addr;
-            // socklen_t addr_size = sizeof(client_addr);
-            // if ((request_accept = accept(0, (struct sockaddr*)&client_addr, &addr_size)) < 0)
-            // {
-            //     std::cout << "accept Error\n";
-            //     exit(-1);
-            // }
-            int num = 1;
-            /* std::function<void(int)> job = [](int num){return num;}; */
-            /* Th.QueueJob(job); */
+    // CREATING THREADPOOL AND STARTING TO LOOK FOR JOBS
+    ThreadPool tp;
+
+    // WHILE THE SERVER IS STILL RUNNING
+    while(!tp.getExit()){
+        // AND THE THE THREADPOOL IS NOT BUSY
+        if(tp.busy()){
+            // CREATE A CLIENT AND WAIT FOR A REQUEST TO BE SENT
+            tp.QueueJob([]{Client client;});
+        }
     }
 
-     Th.Stop();  
+    // CLOSE ALL THREADS
+    tp.Stop();  
+
+    // DISCONNECTS THE SERVER
+    server.closeFD();
 
     return 0;
 }
