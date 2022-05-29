f46e8050ebdfa3747b5a589c85d6e51d2f11ccee
khalilmasri <khalil.1996km@gmail.com>
2022-05-29 15:01:38 +0300

    client header

--- include/client.hpp
+++ include/client.hpp
@@ -4,38 +4,30 @@
 // LIBRARIES
 #include <thread>
 #include <iostream>
+#include <netinet/in.h>
 #include <sys/socket.h>
+//#include <sys/types.h>
+#include  <arpa/inet.h>
+#include <unistd.h>
+
+#include "server.hpp"
+
+extern int server_sock;
 
 #define MAX_TRANSMISSION_LENGTH 100000
 
 class Client{
-
     private:
-        // int client_ID;
-        // bool available_c;
-        // struct sockaddr_in client_addr;
-        // char buff[MAX_TRANSMISSION_LENGTH];
-        // socklen_t addr_size = sizeof(client_addr);
-        // int server_fd;
+        int client_ID;
+        struct sockaddr_in client_addr;
+        char buff[MAX_TRANSMISSION_LENGTH];
+        socklen_t addr_size = sizeof(client_addr);
+
+        void listen_request();
+        void handle_request();
 
     public:
-        // Client(){
-        //     this->client_ID = 0;
-        //     this->available_c = true;
-        // }
-
-        // void listen_request(int serverFD) {
-        //     if ((this->client_ID = accept(serverFD, (struct sockaddr*)&client_addr, &addr_size)) < 0)
-        //     {
-        //         std::cout << "accept Error\n";
-        //         exit(-1);
-        //     }
-        //     this->available_c = false;
-        // }
-        
-        bool get_available() const{
-            // return this->available_c;
-        }
+        Client();
 };
 
 #endif
