7e5bd9e9d63a410ae7ba220a2b3559ebc7bb3365
khalilmasri <khalil.1996km@gmail.com>
2022-05-29 12:43:46 +0300

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,34 @@
 
 #include "../include/exception.hpp"
 #include "../include/server.hpp"
+#include <string>
+#include "../include/threadPool.hpp"
+
 
 int main(int argc, char **argv){
 
     Server server;
     server.run();
+
+    ThreadPool Th;
+    Th.Start();
+
+    
+    while(!Th.getExit()){
+            // int request_accept;
+            // struct sockaddr_in client_addr;
+            // socklen_t addr_size = sizeof(client_addr);
+            // if ((request_accept = accept(0, (struct sockaddr*)&client_addr, &addr_size)) < 0)
+            // {
+            //     std::cout << "accept Error\n";
+            //     exit(-1);
+            // }
+            int num = 1;
+            std::function<void(int)> job = [](int num){return num;};
+            Th.QueueJob(job);
+    }
+
+     Th.Stop();  
+
     return 0;
 }
-
