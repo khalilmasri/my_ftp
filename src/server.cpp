961d55f4531172561275bb830f74594c97fbc79a
khalilmasri <khalil.1996km@gmail.com>
2022-05-29 15:01:59 +0300

    Server:

--- src/server.cpp
+++ src/server.cpp
@@ -1,54 +1,25 @@
 #include "../include/server.hpp"
 #include "../include/threadPool.hpp"
 
+int server_sock;
+
 Server::Server(){
     create_server_sock();
+    run();
 }
 
-/*
-    thread made at beginning 
-*/
-
-Server::~Server(){
-   
-};
+Server::~Server(){};
 
 Server& Server::operator = (const Server& rhs){
     (void)rhs;
     return *this;
 }
 
-int Server::handleClient()  {
-    int bytes;
-    char buff[MAX_TRANSMISSION_LENGTH];
-    int request_accept;
-    struct sockaddr_in client_addr;
-    socklen_t addr_size = sizeof(client_addr);
-    int count = 0;
-    while (1)
-    {
-        if ((request_accept = accept(this->server_sock, (struct sockaddr*)&client_addr, &addr_size)) < 0)
-        {
-            std::cout << "accept Error\n";
-            exit(-1);
-        }
-        std::cout << "Client Addr = " << client_addr.sin_addr.s_addr << std::endl;
-    
-        bytes = read(request_accept, &buff, MAX_TRANSMISSION_LENGTH);
-    //    std::cout << "Message-> " << buff << std::endl;
-        count++;
-        std::cout << count << std::endl;
-    }
-}
-
-int Server::getServerSockID() {
-    return this->server_sock;
-}
-
 void Server::create_server_sock(){
-    if ((this->server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         std::cout << "socket err\n";
+        closeFD();
         exit(-1);
     }
 
@@ -56,31 +27,24 @@ void Server::create_server_sock(){
     this->server_address.sin_family = AF_INET;
     this->server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if((bind(this->server_sock, (struct sockaddr*)&this->server_address, sizeof(this->server_address))) == -1){
+    if((bind(server_sock, (struct sockaddr*)&this->server_address, sizeof(this->server_address))) < 0){
         std::cout << "Failed to bind\n";
+        closeFD();
         exit(1);
     }
 }
 
-// bool Server::create_thread_pool(void)
-// {
-//     for (int i = 0; i < POOL_SIZE; i++)
-//     {
-//         this->clients.push_back(Client(i));
-//     }
-//     this->client_number = POOL_SIZE;
-
-//     return true;
-// }
+void Server::closeFD(){
+    std::cout << "Disconnecting server...\n";
+    close(server_sock);
+}
 
 void Server::run(){
 
-    if (listen(this->server_sock, MAX_THREAD_NUMBER) < 0)
+    if (listen(server_sock, MAX_THREAD_NUMBER) < 0)
     {
         std::cout << "Try again later\n";
         exit (-1);
     }
- 
-        //handleClient();
 }
 
